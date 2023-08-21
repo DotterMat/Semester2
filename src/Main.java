875c78d4f983689ef178d337c0cae2100c24d243
mathias1000zzz <mathqrr@gmail.com>
2023-08-21 09:40:18 +0200

    Strange

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("Hello world 2");
-        System.out.println("");
+        System.out.println("Hello world 3");
     }
 }
